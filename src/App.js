16757649be11e4117b7e297d41b83c63b20e364a
Francisco Llinin <pacoflores1999@gmail.com>
2023-03-12 19:21:12 -0500

    implement redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Provider } from "react-redux";
+import store from "./store";
+
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="text-blue-600">
       APP
     </div>
+    </Provider>
   );
 }
 
