8c64defb81c88738fb0c9fac7dfe9b7278057eb7
Francisco Llinin <pacoflores1999@gmail.com>
2023-03-14 19:25:14 -0500

    Navbar and Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { Provider } from "react-redux";
 import store from "./store";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './containers/Home';
+import Error404 from './containers/errors/Error404'
 
 function App() {
   return (
     <Provider store={store}>
-    <div className="text-blue-600">
-      APP
-    </div>
+    <Router>
+      <Routes>
+        {/* Error display */}
+        <Route path="*" element={<Error404/>} />
+
+        {/* Home */}
+        <Route exact path ='/' element = {<Home/>} />
+
+      </Routes>
+    </Router>
     </Provider>
   );
 }
